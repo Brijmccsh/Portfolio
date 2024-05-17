5bba12b02bbbded5faf682c792805093957fd510
Brijesh Tiwari <69032401+Brijmccsh@users.noreply.github.com>
2024-05-17 11:28:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
   faLinkedin,
@@ -8,23 +8,22 @@ import {
   faFileLines,
 } from "@fortawesome/free-solid-svg-icons";
 
-{/* <div className="container mx-auto p-6"> */}
 const Home = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen" style={{ backgroundColor: "#e6e6fa", color: "#98ff98" }}>
       <div className="text-center md:text-left md:flex justify-center m-auto">
-        <img className="mx-auto w-64 h-64 rounded-full" src="/images/profile.png" /> {/* image */}
+        <img className="mx-auto w-64 h-64 rounded-full" src="/images/profile.png" alt="Profile" /> {/* image */}
         <div className="my-auto p-6 text-lg">
           <div className="font-display font-semibold text-3xl">
-          Brijesh Tiwari
+            Rohit Nema
           </div>
           <div>
-            Computer & Information Science at Syracuse University
+            Computer Science at Syracuse University
           </div>
           <div>
-            Software Engineer || Graduate Researcher
+            Graduate Researcher
           </div>
-          <div className="space-x-4 mt-3" style={{fontSize: 32}}>
+          <div className="space-x-4 mt-3" style={{ fontSize: 32 }}>
             <button className="text-teal-600 hover:text-teal-700 dark:text-teal-400 dark:hover:text-teal-500">
               <a href="mailto:brtiwari@syr.edu">
                 <FontAwesomeIcon
